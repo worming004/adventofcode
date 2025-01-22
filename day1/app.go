9a383e8526b81436a80707ae3d89d70802d26972
worming <worming@hotmail.be>
2025-01-22 19:03:08 +0100

    day1 - first half

---
+++ day1/app.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type pair struct {
+	a, b int
+}
+
+type pairs []pair
+
+type input struct {
+	cola, colb []int
+}
+
+func (i input) toPairs() pairs {
+	sort.Sort(sort.IntSlice(i.cola))
+	sort.Sort(sort.IntSlice(i.colb))
+	result := pairs{}
+	for idx := range i.cola {
+		result = append(result, pair{i.cola[idx], i.colb[idx]})
+	}
+	return result
+}
+
+func (p pairs) distance() int {
+	result := 0
+	for _, p := range p {
+		subr := p.a - p.b
+		if subr < 0 {
+			subr = -subr
+		}
+		result += subr
+	}
+	return result
+}
+
+func main() {
+	input := parse()
+	pairs := input.toPairs()
+	distance := pairs.distance()
+	fmt.Println(distance)
+}
+
+func parse() input {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	result := input{}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		t := scanner.Text()
+		splitted := strings.Split(t, " ")
+		splitted = append(splitted[:1], splitted[3:]...)
+		if len(splitted) != 2 {
+			panic(fmt.Errorf("Invalid input, expected length of 2, got %d", len(splitted)))
+		}
+
+		a, err := strconv.Atoi(splitted[0])
+		if err != nil {
+			panic(err)
+		}
+		b, err := strconv.Atoi(splitted[1])
+		if err != nil {
+			panic(err)
+		}
+		result.cola = append(result.cola, a)
+		result.colb = append(result.colb, b)
+	}
+
+	return result
+}
---
+++ day1/app_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+var i = input{
+	cola: []int{3, 4, 2, 8, 9},
+	colb: []int{3, 4, 2, 5, 7},
+}
+
+func TestToPairs(t *testing.T) {
+	p := i.toPairs()
+	if len(p) != 5 {
+		t.Errorf("Expected 3 pairs, got %d", len(p))
+	}
+	if p[0].a != 2 || p[0].b != 2 {
+		t.Errorf("Expected 1, 1, got %d, %d", p[0].a, p[0].b)
+	}
+	if p[1].a != 3 || p[1].b != 3 {
+		t.Errorf("Expected 2, 2, got %d, %d", p[1].a, p[1].b)
+	}
+	if p[2].a != 4 || p[2].b != 4 {
+		t.Errorf("Expected 3, 3, got %d, %d", p[2].a, p[2].b)
+	}
+}
+
+func TestDistance(t *testing.T) {
+	p := i.toPairs()
+	if p.distance() != 5 {
+		t.Errorf("Expected 5, got %d", p.distance())
+	}
+}
