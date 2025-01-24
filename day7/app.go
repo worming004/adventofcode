a1006638c2ceba922e02c697ed4ef6dee445144f
worming <worming@hotmail.be>
2025-01-24 03:02:27 +0100

    day7 - first half

---
+++ day7/app.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	content, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	parsed := parse(string(content))
+	result := 0
+	for _, l := range parsed {
+		tester := newOperationTester(l.result, l.values)
+		if tester.test() {
+			result += tester.result
+		}
+	}
+	fmt.Println(result)
+}
+
+type line struct {
+	result int
+	values []int
+}
+
+func parse(input string) []line {
+	lines := strings.Split(input, "\n")
+	if lines[len(lines)-1] == "" {
+		lines = lines[:len(lines)-1]
+	}
+	result := make([]line, len(lines))
+	for i, l := range lines {
+		v := strings.Split(l, ":")[0]
+		othersStr := strings.Split(strings.Split(l, ":")[1], " ")
+		if othersStr[0] == "" {
+			othersStr = othersStr[1:]
+		}
+		others := make([]int, len(othersStr))
+		for i, v := range othersStr {
+			others[i] = atoi(v)
+		}
+		result[i] = line{result: atoi(v), values: others}
+	}
+
+	return result
+}
+
+type operator func(int, int) int
+
+func add(a, b int) int {
+	return a + b
+}
+func mult(a, b int) int {
+	return a * b
+}
+
+type operationTester struct {
+	result    int
+	values    []int
+	operators []operator
+}
+
+func newOperationTester(expectedResult int, values []int) *operationTester {
+	return &operationTester{
+		expectedResult,
+		values,
+		[]operator{add, mult},
+	}
+}
+
+func (ot *operationTester) test() bool {
+	return subtest(ot.result, ot.values[0], ot.values[1:], ot.operators)
+}
+
+func subtest(expected, result int, values []int, operators []operator) bool {
+	if len(values) == 0 {
+		return expected == result
+	}
+
+	for _, operator := range operators {
+		if subtest(expected, operator(result, values[0]), values[1:], operators) {
+			return true
+		}
+	}
+	return false
+}
+
+func atoi(v string) int {
+	r, err := strconv.Atoi(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return r
+}
---
+++ day7/app_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_operationTester_test(t *testing.T) {
+	type fields struct {
+		result    int
+		values    []int
+		operators []operator
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{"test1", fields{190, []int{10, 19}, []operator{add, mult}}, true},
+		{"test2", fields{3267, []int{81, 40, 27}, []operator{add, mult}}, true},
+		{"test3", fields{83, []int{14, 5}, []operator{add, mult}}, false},
+		{"test4", fields{156, []int{15, 6}, []operator{add, mult}}, false},
+		{"test5", fields{292, []int{11, 6, 16, 20}, []operator{add, mult}}, true},
+		{"test6", fields{21037, []int{9, 7, 18, 13}, []operator{add, mult}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ot := &operationTester{
+				result:    tt.fields.result,
+				values:    tt.fields.values,
+				operators: tt.fields.operators,
+			}
+			if got := ot.test(); got != tt.want {
+				t.Errorf("operationTester.test() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parse(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []line
+	}{
+		{"test1", args{"10: 19 20 30 40\n"}, []line{{10, []int{19, 20, 30, 40}}}},
+		{"test2", args{"21037: 9 7 18 13\n"}, []line{{21037, []int{9, 7, 18, 13}}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parse(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
